2ece9a75c5f1cd435e507169fe3ba673381b5195
Jena-Biswajit <biswajitjena529@gmail.com>
2025-01-13 20:34:47 +0530

    iteration using itrerator and listiterator

--- src/main/java/com/collections/CollectionIteration.java
+++ src/main/java/com/collections/CollectionIteration.java
@@ -9,5 +9,32 @@ public class CollectionIteration {
         for (String fruit : list) {
             System.out.println(fruit);
         }
+
+        // 1. For loop with index (for list type)
+        System.out.println("\nUsing For loop with index:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        // 2. Using Iterator
+        System.out.println("\nUsing Iterator:");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        // 3. Using ListIterator
+        System.out.println("\nUsing ListIterator:");
+        ListIterator<String> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
+
+        // 4. Using Enumeration
+        System.out.println("\nUsing Enumeration:");
+        Enumeration<String> enumeration = Collections.enumeration(list);
+        while (enumeration.hasMoreElements()) {
+            System.out.println(enumeration.nextElement());
+        }
     }
 }
