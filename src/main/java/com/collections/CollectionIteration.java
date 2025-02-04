ef71c74a989f1174afddae2e354be0f72ba867ad
Jena-Biswajit <biswajitjena529@gmail.com>
2025-02-04 23:37:49 +0530

    Iterate over set, map ,hasPrevious() method

--- src/main/java/com/collections/CollectionIteration.java
+++ src/main/java/com/collections/CollectionIteration.java
@@ -36,5 +36,56 @@ public class CollectionIteration {
         while (enumeration.hasMoreElements()) {
             System.out.println(enumeration.nextElement());
         }
+
+        //5. iterater list
+        List<Integer> iterateList = Arrays.asList(3,0,8);
+        Iterator<Integer>listIterator1 = iterateList.iterator();
+
+        System.out.println("\niterate over list");
+
+        while (listIterator1.hasNext()){
+            System.out.println(listIterator1.next());
+        }
+
+
+        //hasPrevious() example
+        List<String> list1 = Arrays.asList("One", "Two", "Three", "Four");
+
+        // Creating ListIterator starting at the end of the list
+        ListIterator<String> listIterator2 = list1.listIterator(list1.size());
+
+        System.out.println("\nIterating Backward using hasPrevious():");
+        while (listIterator2.hasPrevious()) {
+            System.out.println(listIterator2.previous());
+        }
+
+
+       //6. iterate set
+       Set<Integer> iterateSet = new HashSet<>(Arrays.asList(10,20,30));
+        Iterator<Integer> iterate = iterateSet.iterator();
+
+        System.out.println("\niterate over set");
+
+        while(iterate.hasNext()){
+            System.out.println(iterate.next());
+        }
+
+        //iterate over entrySet of Map
+        Map<Integer,String> iterateMap = new HashMap<>();
+        iterateMap.put(1,"biswa");
+        iterateMap.put(2,"abd");
+        iterateMap.put(3,"bcd");
+        iterateMap.put(4,"efg");
+        iterateMap.put(5,"ijk");
+        iterateMap.put(5,"ijk");
+
+        Iterator<Map.Entry<Integer,String>> mapIterator = iterateMap.entrySet().iterator();
+
+        System.out.println("\niterate over entrySet");
+
+            while (mapIterator.hasNext()) {
+                Map.Entry<Integer, String> entry = mapIterator.next();
+                System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+            }
     }
 }
