127f9bc913d830b206bf12ceaa97dde97315b4f9
Jena-Biswajit <biswajitjena529@gmail.com>
2025-02-23 22:50:33 +0530

    FixedThreadPoolExample.java  added

--- src/main/java/com/thread/FixedThreadPoolExample.java
+++ src/main/java/com/thread/FixedThreadPoolExample.java
@@ -1,2 +1,17 @@
-package com.thread;public class FixedThreadPoolExample {
+package com.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FixedThreadPoolExample {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        for (int i = 1; i <= 5; i++) {
+            final int taskId = i;
+            executor.execute(() -> System.out.println("Task " + taskId + " executed by " + Thread.currentThread().getName()));
+        }
+
+        executor.shutdown();
+    }
 }
