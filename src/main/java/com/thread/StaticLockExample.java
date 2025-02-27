6e53d5986ec51ae5e3800424fb6d8dedef5ac8c9
Jena-Biswajit <biswajitjena529@gmail.com>
2025-02-27 23:05:11 +0530

    StaticLockExample.java  added

--- src/main/java/com/thread/StaticLockExample.java
+++ src/main/java/com/thread/StaticLockExample.java
@@ -1,2 +1,24 @@
-package com.thread;public class StaticLockExample {
+package com.thread;
+
+class SharedStatic {
+    synchronized static void printNumbers(int n) {  // Lock on SharedStatic.class
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - " + (n * i));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class StaticLockExample {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> SharedStatic.printNumbers(5), "Thread 1");
+        Thread t2 = new Thread(() -> SharedStatic.printNumbers(10), "Thread 2");
+
+        t1.start();
+        t2.start();
+    }
 }
