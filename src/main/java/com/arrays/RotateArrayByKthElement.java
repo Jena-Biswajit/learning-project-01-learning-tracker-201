18d59b12ac1a18a0149dc992aac7345e9258793c
Jena-Biswajit <biswajitjena529@gmail.com>
2025-03-05 14:19:16 +0530

    RotateArrayByKthElement

---
+++ src/main/java/com/arrays/RotateArrayByKthElement.java
@@ -0,0 +1,32 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+class RotateArray {
+
+    public void rotateArr(int[] array, int position) {
+        // result will be 5,7,83,2,1,23,4
+        int len = array.length;
+        for (int j = 0; j < position; j++) {
+            for (int i = 0; i < len; i++) {
+
+                int temp;
+                temp = array[i];
+                array[i] = array[len - 1];
+                array[len - 1] = temp;
+            }
+        }
+        System.out.println("array after rotate " + Arrays.toString(array));
+    }
+}
+
+public class RotateArrayByKthElement {
+    public static void main(String[] args) {
+        int[] arr = { 1, 23, 4, 5, 7, 83, 2 };
+
+        int rotateElement = 3;
+
+        RotateArray ob1 = new RotateArray();
+        ob1.rotateArr(arr, rotateElement);
+    }
+}
