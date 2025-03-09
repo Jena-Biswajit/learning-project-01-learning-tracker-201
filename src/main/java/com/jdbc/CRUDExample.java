39da604318683c34c38ce25171703d87c99c9c0f
Jena-Biswajit <biswajitjena529@gmail.com>
2025-03-09 22:49:19 +0530

    CRUDExample.java added

--- src/main/java/com/jdbc/CRUDExample.java
+++ src/main/java/com/jdbc/CRUDExample.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class CRUDExample {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/mydb";
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
         String user = "root";
         String password = "subhasmita";
 
@@ -13,7 +13,7 @@ public class CRUDExample {
             Statement stmt = conn.createStatement();
 
             // CREATE (Insert)
-            String insertQuery = "INSERT INTO users (id, name, email) VALUES (1, 'John Doe', 'john@example.com')";
+            String insertQuery = "INSERT INTO users (id, name, email) VALUES (1, 'Biswajit Jena', 'biswajitjena529.com')";
             stmt.executeUpdate(insertQuery);
 
             // READ (Select)
@@ -23,7 +23,7 @@ public class CRUDExample {
             }
 
             // UPDATE
-            String updateQuery = "UPDATE users SET name='Jane Doe' WHERE id=1";
+            String updateQuery = "UPDATE users SET name='Biswajit Jena' WHERE id=1";
             stmt.executeUpdate(updateQuery);
 
             // DELETE
