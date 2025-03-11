b0109615db1217d4bab47f16d359ed8d10d1067d
Jena-Biswajit <biswajitjena529@gmail.com>
2025-03-11 23:21:13 +0530

    Revert "Merge remote-tracking branch 'origin/bugfix/arrays' into bugfix/arrays"
    This reverts commit 99705d5808a960e109d354691235f649cc62d2d0, reversing
    changes made to c8177f367a3b723c7487db86ee9c56dafc2816db.

--- src/main/java/com/arrays/segregatevenOddElements.java
+++ src/main/java/com/arrays/segregatevenOddElements.java
@@ -1,44 +1,40 @@
 package com.arrays;
 
-public class segregatevenOddElements {
+class GfG {
 
-    // Function to move all -ve element to end of array
+    // Function to move all -ve element to end of array 
     // in same order.
     static void segregateElements(int[] arr) {
+        int n = arr.length;
 
-        // segregate negative element in order
-        // result would be -1 -3 -2 1 7 5 11 6 or
-        // 1 7 5 11 6 -1 -3 -2
+        // Create an empty array to store result
+        int[] temp = new int[n];
+        int idx = 0;
 
-        int negIndex = 0; // Pointer for negative numbers
-
-        for (int posIndex = 0; posIndex < arr.length; posIndex++) {
-            if (arr[posIndex] < 0) {
-                // If negative number found, shift elements right and insert at negIndex
-                int negNumber = arr[posIndex];
-                int tempIndex = posIndex;
-
-                while (tempIndex > negIndex) {
-                    arr[tempIndex] = arr[tempIndex - 1];
-                    tempIndex--;
-                }
-
-                arr[negIndex] = negNumber;
-                negIndex++; // Move negative index forward
-            }
-            // Use 'elseif' to ensure that positive numbers are left untouched
-            else if (arr[posIndex] >= 0) {
-                // Simply move to the next element
-                continue;
-            }
-
+        // First fill non-negative elements into the 
+        // temporary array
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= 0)
+                temp[idx++] = arr[i];
         }
+
+        // Now fill negative elements into the 
+        // temporary array
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0)
+                temp[idx++] = arr[i];
+        }
+
+        // copy the elements from temp to arr
+        System.arraycopy(temp, 0, arr, 0, n);
     }
+
     public static void main(String[] args) {
-        int[] arr = { 1, -1, -3, 7, 5, 11, 6, -2 };
+        int[] arr = {1, -1, -3, -2, 7, 5, 11, 6};
         segregateElements(arr);
 
-        for (int ele : arr)
+        for (int ele: arr)
             System.out.print(ele + " ");
     }
 }
+
