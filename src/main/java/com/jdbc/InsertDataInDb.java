d1eb09d934031fb460713541a524424e5c1bc783
Jena-Biswajit <biswajitjena529@gmail.com>
2025-03-11 15:55:21 +0530

    InsertDataInDb using statement interface

---
+++ src/main/java/com/jdbc/InsertDataInDb.java
@@ -0,0 +1,37 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class InsertDataInDb {
+    static String url = "jdbc:mysql://localhost:3306/mydatabase";
+    static String user = "root";
+    static String password = "Bbiswajit@408";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+
+            String query = String.format("INSERT INTO students(name,age,marks) VALUES('%s',%o,%f)", "Satya", 26, 74.32);
+
+            int rowsAffected = stmt.executeUpdate(query);
+            if (rowsAffected > 0) {
+                System.out.println("data inserted successfully");
+            } else {
+                System.out.println("data not inserted ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
