84d5d47eeb335ecc69889544ac55f553fdac9554
Jena-Biswajit <biswajitjena529@gmail.com>
2025-03-11 15:57:21 +0530

    UpdateDataInTable using statement interface '

---
+++ src/main/java/com/jdbc/UpdateDataInTable.java
@@ -0,0 +1,37 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UpdateDataInTable {
+    static String url = "jdbc:mysql://localhost:3306/mydatabase";
+    static String user = "root";
+    static String password = "Bbiswajit@408";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement stmt = conn.createStatement();
+
+            String query = String.format("UPDATE students SET MARKS = %f WHERE id = %d", 89.4, 2);
+
+            int rowsAffected = stmt.executeUpdate(query);
+            if (rowsAffected > 0) {
+                System.out.println("data updated successfully");
+            } else {
+                System.out.println("data not updated ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
