5d143f8e1e484d5985fde6beabc3ff9a71bf228c
Jena-Biswajit <biswajitjena529@gmail.com>
2025-03-18 22:05:20 +0530

    TwoSum return index of target sum, implement in leetcode easy

--- src/main/java/com/arrays/TwoSum.java
+++ src/main/java/com/arrays/TwoSum.java
@@ -1,31 +1,31 @@
 package com.arrays;
 
+import java.util.Arrays;
+
 public class TwoSum {
-   static class Solution {
+    static class Solution {
         public int[] twoSum(int[] nums, int target) {
-    
-           int len = nums.length;
-           int start =0  , end =len ;
-           
-    
-           for( start =0 ; start<len-1 ;start++  ){
-                for(end = len ; end>0 ;end--  ){
-    
-                    if((nums[start] + nums[end]) == target){
-                        break;
+
+            int len = nums.length;
+
+            for(int i =0 ; i<len ; i++){
+                for (int j = i+1 ; j<len;j++){
+                    if(nums[i]+nums[j]==target){
+                        return new int[]{i,j};
                     }
-                }   
-           }
-           return new int[]{start, end};
+                }
+
+            }
+            return new int[]{};
         }
     }
 
-
     public static void main(String[] args) {
-        int [] array = {2,7,11,9};
+        int[] array = {2, 11, 7, 9};
         int target = 9;
 
         Solution ob1 = new Solution();
-        int [] resultArray =ob1.twoSum(array,target);
+        int[] resultArray = ob1.twoSum(array, target);
+        System.out.println(Arrays.toString(resultArray));
     }
 }
