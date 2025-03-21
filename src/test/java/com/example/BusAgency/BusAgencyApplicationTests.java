ba363f9a6df21034396fbe433ba49053054b52eb
Jena-Biswajit <biswajitjena529@gmail.com>
2025-03-21 20:16:47 +0530

    added maven-wrapper for spring boot maven setup

---
+++ src/test/java/com/example/BusAgency/BusAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusAgency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
