e0c5c7871425715ea65cbaef78198aa297be7c78
Jena-Biswajit <biswajitjena529@gmail.com>
2025-05-05 00:14:22 +0530

    UsingMorethanOneConstructors in java

---
+++ src/main/java/com/example/methodexample/MethodArgumentExample.java
@@ -0,0 +1,15 @@
+//package com.example.methodexample;
+//
+//public class MethodArgumentExample {
+//
+//    // passing different types of arguments
+//
+//    private void employeeDetails(){
+//        int id = 1 ;
+//        int Studentclass = 8;
+//        String name ;
+//        int
+//    }
+//
+//
+//}
